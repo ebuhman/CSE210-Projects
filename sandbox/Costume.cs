86b5f92c0287a621d1ba3ea748ac189e9772c341
ElucidFGC <elucidvt@gmail.com>
2025-10-01 10:18:09 -0600

    Learning Prep W03

---
+++ sandbox/Costume.cs
@@ -0,0 +1,17 @@
+class Costume
+{
+    // Attributes
+    string outfit="";
+    string footwear="";
+    string tools="";
+    string size="";
+
+
+    // Behaviors
+    void Display()
+    {
+        
+    }
+
+
+}
---
+++ sandbox/Program.cs
@@ -0,0 +1,12 @@
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Costume myCostume = new Costume();
+        myCostume.outfit = "Tartan and kilt";
+        myCostume.Display();
+    }
+
+
+}
